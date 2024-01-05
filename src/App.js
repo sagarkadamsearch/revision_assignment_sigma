0d24f48ac0bddbada3e968e88975687c29834513
sagarkadamsearch <sagarkadamsearch@gmail.com>
2024-01-05 21:11:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
-// import Admin from './Pages/Admin';
+import Admin from "./Pages/Admin"
 import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
-     {/* <Admin/> */}
+     <Admin/>
      <Signup/>
     </div>
   );
